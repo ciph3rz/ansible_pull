601eb9c95d12900b11c87138e3a1fc3a8c78fc95
Ciph3rz <ciph3rz@tribal.odyssey.com>
2021-12-22 14:33:22 -0500

    init bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+printf "Bootstrap Script\n"
+
+printf "Which Ansible Play would you like to run?\n"
+printf "For example core, desktop or vps?\n "
+
+read -r play
+
+mkdir -p $HOME/code
+cd $HOME/code
+
+printf "Installing pre-requisites\n"
+
+sudo apt install git curl wget ansible
+
+printf "Cloning Ansible Library\n"
+git clone https://github.com/ciph3rz/ansible_pull.git
+
+cd $HOME/code/ansible_pull
+
+printf "Running Ansible Playbook $play \n"
+ansible-playbook $play.yml --ask-become-pass
