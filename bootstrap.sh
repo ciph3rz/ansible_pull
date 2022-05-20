0eb98868abfc8ce9fa2ce98a3702df9c36072770
edworks <edworks@tribal-odyssey.com>
2022-05-20 10:47:00 -0400

    updated logic to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 . /etc/os-release
 
+GHREPO=https://github.com/ciph3rz/ansible_pull.git
+CODEREPO=$HOME/code
+LOCALREPO=$CODEREPO/ansible_pull
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
 printf "Bootstrap Script\n"
 
 printf "Which Ansible Play would you like to run?\n"
@@ -8,8 +13,15 @@ printf "For example core, desktop, kali or vps?\n "
 
 read -r play
 
-mkdir -p $HOME/code
-cd $HOME/code
+printf "Ensure Code Repo exists prior to running script"
+if [ ! -d $CODEREPO ]
+then
+    mkdir -p $CODEREPO
+fi
+
+
+mkdir -p $CODEREPO
+cd $CODEREPO
 
 printf "Installing pre-requisites\n"
 
@@ -24,13 +36,20 @@ else
 fi
 
 printf "Cloning Ansible Library\n"
-if [ -d $HOME/code/ansible_pull ]; then
-    git pull https://github.com/ciph3rz/ansible_pull.git
+# if [ -d $LOCALREPO ]; then
+#     git pull https://github.com/ciph3rz/ansible_pull.git
+# else
+#     git clone https://github.com/ciph3rz/ansible_pull.git
+# fi
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $GHREPO $LOCALREPO
 else
-    git clone https://github.com/ciph3rz/ansible_pull.git
+    cd $LOCALREPO
+    git pull $GHREPO
 fi
 
-cd $HOME/code/ansible_pull
+cd $LOCALREPO
 
 printf "Running Ansible Playbook $play \n"
 ansible-playbook $play.yml --ask-become-pass
